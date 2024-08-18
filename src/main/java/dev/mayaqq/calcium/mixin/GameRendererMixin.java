23a963d5635974d7db7e883d34f9ff53ac62993e
Derpy <57187871+DerpDerpling@users.noreply.github.com>
2024-08-17 20:07:56 -0400

    fix github weirdness and rename jar

--- src/main/java/dev/mayaqq/calcium/mixin/GameRendererMixin.java
+++ src/main/java/dev/mayaqq/calcium/mixin/GameRendererMixin.java
@@ -1,9 +1,5 @@
 package dev.mayaqq.calcium.mixin;
 
-<<<<<<< Updated upstream
-import com.mojang.blaze3d.vertex.PoseStack;
-=======
->>>>>>> Stashed changes
 import net.minecraft.client.renderer.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,13 +8,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
-<<<<<<< Updated upstream
-    @Inject(method = "renderLevel", at = @At("HEAD"), cancellable = true)
-    private void render(float f, long l, PoseStack poseStack, CallbackInfo ci) {
-=======
     @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
     private void render(CallbackInfo ci) {
->>>>>>> Stashed changes
         ci.cancel();
     }
 }
